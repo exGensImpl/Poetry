60580fd98a8fba6b4a8d6183d21b5ed7da867846
KVANT\mk <zzz3@zzz.com>
2020-01-13 18:25:02 +0300

    The Rhytm class has been translated to Kotlin

--- src/kotlin/main.kt
+++ src/kotlin/main.kt
@@ -1,8 +1,45 @@
+package exgens.poetry
+
+import java.io.*
+
 fun main(args: Array<String>) {
     println("Hello, Kotlin!")
 }
 
-data class Word(val text: String, val rhytm: Rhytm) { }
+data class Word(val text: String, val rhytm: Rhythm) { }
 
-class Rhytm(private val _length: Int, private val _stress_indicies: Sequence<Int>) { 
+class Rhythm(val length: Int, private val stressIndicies: Sequence<Int>) { 
+
+    val hasStress = stressIndicies.any()
+
+    val isStressed = stressIndicies.contains(0)
+
+    val isEmpty = length == 0
+
+    companion object{
+        fun concat(rhythms: Sequence<Rhythm>)
+        {
+            val stresses =  mutableListOf<Int>()
+            var length = 0
+
+            for (rhythm in rhythms) {
+                for (stress in rhythm.stressIndicies) {
+                    stresses.add(stress + length);
+                }
+                length += rhythm.length;
+            }
+
+            Rhythm(length, stresses.asSequence())
+        }
+    }
+
+    fun shifted(syllables : Int = 1)
+        = Rhythm(
+                if (length >= syllables) length - syllables else 0,
+                stressIndicies.map{ it - syllables }.filter{ it >= 0 })
+    
+    override fun toString()
+        = (1..length)
+                    .map { if (stressIndicies.contains(it)) '\'' else '-' }
+                    .joinToString("")
 }
